7455dd1e35008f4e96d61235a0ff2c50e1f14a79
kellseyvdongen <kellseyvandongen2007@gmail.com>
2026-04-09 12:16:06 +0200

    continued with the rent wheelchair page, dialog added

--- Webservice/actions.php
+++ Webservice/actions.php
@@ -6,96 +6,165 @@ function getMuseums(): array
             "id" => 1,
             "name" => "Rijksmuseum",
             "place" => "Amsterdam",
-            "wheelchairs" => 5,
-            "img" => "./images/rijks.jpg",
-            "url" => "https://www.rijksmuseum.nl/nl"
+            "img" => "./images/rijks.jpg"
     ],
         [
             "id" => 2,
+            "name" => "Van Gogh Museum",
+            "place" => "Amsterdam",
+            "img" => "./images/vanGogh.jpg"
+        ],
+        [
+            "id" => 3,
+            "name" => "Stedelijk Museum",
+            "place" => "Amsterdam",
+            "img" => "./images/stedelijk.jpg"
+        ],
+        [
+            "id" => 4,
+            "name" => "Maritiem Museum",
+            "place" => "Rotterdam",
+            "img" => "./images/maritiem.webp"
+        ],
+        [
+            "id" => 5,
+            "name" => "Openluchtmuseum",
+            "place" => "Arnhem",
+            "img" => "./images/openlucht.jpg"
+        ],
+        [
+            "id" => 6,
+            "name" => "Kunsthal",
+            "place" => "Rotterdam",
+            "img" => "./images/kunsthal.jpg"
+        ],
+        [
+            "id" => 7,
+            "name" => "Paleis het Loo",
+            "place" => "Apeldoorn",
+            "img" => "./images/hetloo.webp"
+        ],
+        [
+            "id" => 8,
+            "name" => "Naturalis",
+            "place" => "Leiden",
+            "img" => "./images/naturalis.jpg"
+        ],
+        [
+            "id" => 9,
+            "name" => "Nijntje Museum",
+            "place" => "Utrecht",
+            "img" => "./images/nijntje.webp"
+        ],
+        [
+            "id" => 10,
+            "name" => "Luchtvaartmuseum Aviodrome",
+            "place" => "Lelystad",
+            "img" => "./images/aviodrome.webp"
+        ],
+        [
+            "id" => 11,
+            "name" => "MORE museum",
+            "place" => "Gorssel",
+            "img" => "./images/more.jpg"
+        ]
+    ];
+}
+
+function getMuseumsDetails($id): array|false
+{
+    $tags = [
+        1 => [
+            "name" => "Rijksmuseum",
+            "place" => "Amsterdam",
+            "wheelchairs" => 5,
+            "img" => "./images/rijks.jpg",
+            "url" => "https://www.rijksmuseum.nl/nl"
+        ],
+        2 => [
             "name" => "Van Gogh Museum",
             "place" => "Amsterdam",
             "wheelchairs" => 4,
             "img" => "./images/vanGogh.jpg",
             "url" => "https://www.vangoghmuseum.nl/nl"
         ],
-        [
-            "id" => 3,
+        3 => [
             "name" => "Stedelijk Museum",
             "place" => "Amsterdam",
             "wheelchairs" => 5,
             "img" => "./images/stedelijk.jpg",
             "url" => "https://www.stedelijk.nl/nl"
         ],
-        [
-            "id" => 4,
+        4 => [
             "name" => "Maritiem Museum",
             "place" => "Rotterdam",
             "wheelchairs" => 3,
             "img" => "./images/maritiem.webp",
             "url" => "https://maritiemmuseum.nl"
         ],
-        [
-            "id" => 5,
+        5 => [
             "name" => "Openluchtmuseum",
             "place" => "Arnhem",
             "wheelchairs" => 2,
             "img" => "./images/openlucht.jpg",
             "url" => "https://www.openluchtmuseum.nl"
         ],
-        [
-            "id" => 6,
+        6 => [
             "name" => "Kunsthal",
             "place" => "Rotterdam",
             "wheelchairs" => 5,
             "img" => "./images/kunsthal.jpg",
             "url" => "https://www.kunsthal.nl"
         ],
-        [
-            "id" => 7,
+        7 => [
             "name" => "Paleis het Loo",
             "place" => "Apeldoorn",
             "wheelchairs" => 3,
             "img" => "./images/hetloo.webp",
             "url" => "https://paleishetloo.nl/"
         ],
-        [
-            "id" => 8,
+        8 => [
             "name" => "Naturalis",
             "place" => "Leiden",
             "wheelchairs" => 2,
             "img" => "./images/naturalis.jpg",
             "url" => "https://www.naturalis.nl"
         ],
-        [
-            "id" => 9,
+        9 => [
             "name" => "Nijntje Museum",
             "place" => "Utrecht",
             "wheelchairs" => 5,
             "img" => "./images/nijntje.webp",
             "url" => "https://nijntjemuseum.nl/"
         ],
-        [
-            "id" => 10,
+        10 => [
             "name" => "Luchtvaartmuseum Aviodrome",
             "place" => "Lelystad",
             "wheelchairs" => 4,
             "img" => "./images/aviodrome.webp",
             "url" => "https://www.aviodrome.nl/"
         ],
-        [
-            "id" => 11,
+        11 => [
             "name" => "MORE museum",
             "place" => "Gorssel",
             "wheelchairs" => 2,
             "img" => "./images/more.jpg",
             "url" => "https://www.museummore.nl"
         ]
-    ];
+        ];
+    return $tags[$id] ?? false;
 }
 
-$data = getMuseums();
 
+if (!isset($_GET['id'])) {
+    $data = getMuseums();
+} else {
+    $data = getMuseumsDetails($_GET['id']);
+}
+
+//Set the header & output JSON so the client will know what to expect.
 header("Content-Type: application/json");
 echo $data !== false ? json_encode($data) : json_encode(['error' => 'Not found']);
+
 
 
--- js/rent.js
+++ js/rent.js
@@ -1,5 +1,11 @@
 const museumsContainer = document.querySelector(".list-museums")
-
+const dialog = document.querySelector("dialog")
+const detailsContainer = document.querySelector(".details")
+const detailNameContainer = document.querySelector(".museum-name")
+const closeDetails = document.querySelector("dialog button")
+closeDetails.addEventListener("click", () => {
+    dialog.close()
+})
 function loadMuseums(url, succesHandler) {
     fetch(url)
         .then (res => {
@@ -18,6 +24,8 @@ function showMuseums(data) {
         const museumDiv = document.createElement("div");
         museumDiv.classList.add("museum-card");
 
+        museumDiv.id = museum.id
+
         const name = document.createElement("h3")
         name.innerText = museum.name
         const img = document.createElement("img")
@@ -30,12 +38,96 @@ function showMuseums(data) {
         museumDiv.append(place)
         museumsContainer.append(museumDiv)
 
-        museumsContainer.addEventListener("click", hirePopup)
+        museumsContainer.addEventListener("click", loadHire)
     }
 }
 
-function hirePopup() {
+function loadHire(e) {
+    console.log(e.target.id)
+    if (e.target.className === "museum-card") {
+        loadMuseums(`./webservice/actions.php?id=${e.target.id}`, hirePopup);
+    }
 
+}
+
+function hirePopup(details) {
+    console.log(details);
+
+    dialog.showModal()
+    detailsContainer.innerHTML = '';
+    detailNameContainer.innerHTML = '';
+
+    const nameDetail = document.createElement('h1');
+    nameDetail.innerText = details.name
+    detailNameContainer.append(nameDetail)
+
+    const leftDetail = document.createElement('div');
+    leftDetail.classList.add('left-detail')
+    const wheelchairsAmount = document.createElement ('p')
+    wheelchairsAmount.innerText = `Aantal rolstoelen beschikbaar per dag: ${details.wheelchairs}`
+
+    const hireForm = document.createElement('form')
+    const formTitle = document.createElement('h2')
+    formTitle.innerText = 'Huur uw Speak-up rolstoel'
+    const labelName = document.createElement("label")
+    labelName.innerText = 'Voor- & Achternaam'
+    labelName.htmlFor = 'name'
+    const fillName = document.createElement("input")
+    fillName.type = 'text'
+    fillName.name = 'name'
+    fillName.placeholder = "vul uw naam in"
+    const labelPhone = document.createElement('label')
+    labelPhone.innerText = 'Telefoonnummer'
+    labelName.htmlFor = 'number'
+    const fillPhone = document.createElement("input")
+    fillPhone.type = 'tel'
+    fillPhone.name = 'number'
+    fillPhone.placeholder = 'vul uw telefoonnummer in'
+    const labelEmail = document.createElement("label")
+    labelEmail.innerText = 'E-mail'
+    labelEmail.htmlFor = 'email'
+    const fillEmail = document.createElement('input')
+    fillEmail.type = 'text'
+    fillEmail.name = 'email'
+    fillEmail.placeholder = 'vul uw e-mail in'
+    const labelDate = document.createElement("label")
+    labelDate.innerText = 'Datum'
+    labelDate.htmlFor = 'date'
+    const fillDate = document.createElement("input")
+    fillDate.type = 'date'
+    fillDate.name = 'date'
+    const submitBtn = document.createElement('button')
+    submitBtn.type = 'submit'
+    submitBtn.innerText = 'Versturen'
+
+    hireForm.append(formTitle)
+    hireForm.append(labelDate)
+    hireForm.append(fillDate)
+    hireForm.append(labelName)
+    hireForm.append(fillName)
+    hireForm.append(labelPhone)
+    hireForm.append(fillPhone)
+    hireForm.append(labelEmail)
+    hireForm.append(fillEmail)
+    hireForm.append(submitBtn)
+
+    leftDetail.append(wheelchairsAmount)
+    leftDetail.append(hireForm)
+
+    const rightDetail = document.createElement('div')
+    rightDetail.classList.add('right-detail')
+    const image = document.createElement('img')
+    image.src = details.img
+
+    const websiteMuseum = document.createElement("a")
+    websiteMuseum.innerText = 'Entree tickets kopen'
+    websiteMuseum.href = details.url
+
+    rightDetail.append(image)
+    rightDetail.append(websiteMuseum)
+
+    detailsContainer.append(leftDetail)
+    detailsContainer.append(rightDetail)
 }
 function errorHandler(error) {
     console.log(error);
